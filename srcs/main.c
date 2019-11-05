c1fd92e55f9ed45778ded5463710b4168301a0f3
Георгий Черний <you@iMac-Georgij.local>
2019-11-05 10:34:02 +0300

    hi

--- srcs/main.c
+++ srcs/main.c
@@ -14,44 +14,62 @@
 #include <stdio.h>
 int		valid(char *s);
 
-int		part1(char *s)
+int		part1(char *s, char sim)
 {
 	int 	count;
-	int 	flag;
+	int 	e;
 	int		i;
 	int 	j;
 
-	count = 1;
+	count = 0;
 	i = 0;
 	j = 0;
-	flag = 0;
+	e = 0;
 	while (s[i] != '#')
 		i++;
-	while (j < 3)
-	{
-		if (s[i] == s[i + 1])
-		{
-			i++;
-			count++;
-		}
-		else if (s[i] == s[i + 5])
-		{
-			i += 5;
-			count++;
-		}
-		ft_putnbr(count);
-		j++;
-	}
-	return ((count == 4)) ? 0 : -1;
+	e = i;
+	while ((s[e + 1] == '#') || (s[e + 5] == '#' || s[e] == '#')) {
+	    s[e] = sim;
+//      ft_putchar('O');
+        count++;
+ //     ft_putnbr(e);
+        i = e;
+        while (s[i + 5] == '#') {
+            i += 5;
+ //         ft_putchar('U');
+            j = i;
+            while (s[j + 1] == '#') {
+                j++;
+                s[j] = sim;
+ //             ft_putchar('L');
+                count++;
+                }
+                j = i;
+            while (s[j - 1] == '#') {
+ //             ft_putchar('M');
+                j--;
+                s[j] = sim;
+                count++;
+                }
+                s[i] = sim;
+                count++;
+        }
+        e++;
+     }
+    printf("count - %d\n", count);
+	return ((count != 4)) ? -1 : 0;
 }
 
 int		main(void)
 {
 	char *s;
 	char buf[100];
+	char sim;
 	int fd;
 	int r;
+	int i;
 
+	i = 0;
 	fd = open("testease", O_RDONLY);
 	s = ft_strnew(1);
 	while ((r = (read(fd, buf, 100))))
@@ -61,7 +79,13 @@ int		main(void)
 	}
 	if (valid(s) < 0)
 		return (-1);
-	ft_putnbr(part1(s));
+	sim = 'A';
+	while (s[i]) {
+        part1(&s[i], sim++);
+        i += 21;
+ //       ft_putchar('V');
+    }
+	ft_putstr(s);
 	return (0);
 }
 
