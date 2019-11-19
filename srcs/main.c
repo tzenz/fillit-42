c31ba03f2742e47ad9ae923b2fc1cabcbb52db4e
Tobbot Zenz <tzenz@ox-f5.21-school.ru>
2019-11-19 17:10:00 +0300

    hi

--- srcs/main.c
+++ srcs/main.c
@@ -34,95 +34,11 @@ int		main(void)
 	head = maintet(s);
 	while (head->next != NULL)
 	{
-		printf("%s\nx - %zu y - %zu\n", *head->content, head->x, head->y);
+		ft_putsstr(head->content);
+		printf("x - %zu y - %zu numb - %zu\n", head->x, head->y, head->numb);
 		head = head->next;
 	}
-	printf("%s\nx - %zu y - %zu\n", *head->content, head->x, head->y);
+	ft_putsstr(head->content);
+	printf("x - %zu y - %zu numb - %zu\n", head->x, head->y, head->numb);
 	return (0);
 }
-
-/*
-	while (head->next != NULL)
-	{
-		printf("%s\nx - %zu y - %zu\n", *head->content, head->x, head->y);
-		head = head->next;
-	}
-	printf("%s\nx - %zu y - %zu\n", *head->content, head->x, head->y);
-
- tetrimino	*maintet(char *s)
-{
-	tetrimino	*head;
-	tetrimino	*tmp;
-	int			i;
-	int 		count;
-	int 		stop;
-
-	i = 0;
-	count = 1;
-	head = NULL;
-	stop = ft_strlen(s);
-	while (s[i] && i < stop)
-	{
-		if (!head)
-		{
-			head = ft_newtet(ft_add(&s[i]), count);
-			tmp = head;
-		} else
-		{
-			tmp->next = ft_newtet(ft_add(&s[i]), ++count);
-			tmp = tmp->next;
-		}
-		i += 21;
-	}
-	return (head);
-}
-
-char   *ft_add(char *s)
-{
-	char 	*buf;
-	int 	sim;
-	int     i;
-	int 	j;
-
-	buf = ft_strnew(10);
-	j = 0;
-	i = 0;
-	sim = 0;
-	while (s[i] && i < 20)
-	{
-		if ((sim = ft_search(&s[i])))
-		{
-			printf("%c\n", sim);
-			while (s[i] && s[i] != '\n')
-			{
-				if (s[i] == sim || s[i + 5] == sim || s[i + 10] == sim)
-					buf[j++] = s[i];
-				else if (s[i - 5] == sim || s[i - 10] == sim)
-					buf[j++] = s[i];
-				i++;
-			}
-			buf[j++] = '\n';
-			sim = 0;
-		}
-		else
-			i += 4;
-		i++;
-	}
-//	ft_putstr(buf);
-	return (buf);
-}
-
-int		ft_search(char *s)
-{
-	int sim;
-	int i;
-	i = 0;
-
-	while (s[i] && s[i] != '\n')
-	{
-		if (s[i] != '.')
-			sim = s[i];
-		i++;
-	}
-	return (sim);
-}*/
--- srcs/newtet.c
+++ srcs/newtet.c
@@ -1,40 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   newtet.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tzenz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:37:14 by tzenz             #+#    #+#             */
-/*   Updated: 2019/10/30 14:37:16 by tzenz            ###   ########.fr       */
+/*   Updated: 2019/11/19 11:31:14 by tzenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/include/libft.h"
 #include <stdio.h>
 
-tetrimino	*maintet(char *s)
+tetrimino		*maintet(char *s)
 {
 	tetrimino	*head;
 	tetrimino	*tmp;
 	int			i;
-	int 		count;
-	int 		stop;
+	int			c;
+	int			stop;
 
 	i = 0;
-	count = 1;
+	c = 1;
 	head = NULL;
 	stop = ft_strlen(s);
 	while (s[i] && i < stop)
 	{
 		if (!head)
 		{
-			head = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]),  count);
+			head = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]), c++);
 			tmp = head;
 		}
 		else
 		{
-			tmp->next = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]),count);
+			tmp->next = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]), c++);
 			tmp = tmp->next;
 		}
 		i += 21;
@@ -42,111 +42,29 @@ tetrimino	*maintet(char *s)
 	return (head);
 }
 
-char   **ft_add(char *s)
+char			**ft_add(char *s)
 {
-	char	**ho;
-	char 	*buf;
-	int 	sim;
-	int     i;
-	int 	j;
+	char		**ho;
+	char		*buf;
+	int			sim;
 
 	buf = ft_strnew(10);
-	j = 0;
-	i = 0;
-	sim = 0;
-	while (s[i] && i < 20)
-	{
-		while (s[i] && s[i] != '\n')
-		{
-			if (s[i] != '.')
-				sim = s[i];
-			i++;
-		}
-		if (sim)
-		{
-			i -= 4;
-			while (s[i] && s[i] != '\n')
-			{
-				if (s[i] == sim || s[i + 5] == sim || s[i + 10] == sim)
-					buf[j++] = s[i];
-				else if (s[i - 5] == sim || s[i - 10] == sim)
-					buf[j++] = s[i];
-				i++;
-			}
-			buf[j++] = '\n';
-			sim = 0;
-		}
-		i++;
-	}
-	ho = ft_strsplit(buf, '\n');
+	ho = ft_strsplit(ft_add_2(s, buf, sim), '\n');
+	free(buf);
 	return (ho);
 }
 
-tetrimino   *ft_newtet(char **s, int	x, int y, int	numb)
+char			*ft_add_2(char *s, char *buf, int sim)
 {
-	tetrimino   *tmp;
-
-	tmp = (tetrimino*)ft_memalloc(sizeof(tetrimino));
-	tmp->numb = numb;
-	tmp->x = x;
-	tmp->y = y;
-	tmp->content = s;
-	tmp->next = NULL;
-	return (tmp);
-
-}
-
-/*tetrimino	*maintet(char *s)
-{
-	tetrimino	*head;
-	tetrimino	*tmp;
 	int			i;
-	int 		count;
-	int 		stop;
+	int			j;
 
-	i = 0;
-	count = 1;
-	head = NULL;
-	stop = ft_strlen(s);
-	while (s[i] && i < stop)
-	{
-		if (!head)
-		{
-			head = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]),  count);
-			tmp = head;
-		}
-		else
-		{
-			tmp->next = ft_newtet(ft_add(&s[i]), ft_x(&s[i]), ft_y(&s[i]),count);
-			tmp = tmp->next;
-		}
-		i += 21;
-	}
-	return (head);
-}
-
-char   *ft_add(char *s)
-{
-	char 	*buf;
-	int 	sim;
-	int     i;
-	int 	j;
-
-	buf = ft_strnew(10);
 	j = 0;
 	i = 0;
-	sim = 0;
 	while (s[i] && i < 20)
 	{
-		while (s[i] && s[i] != '\n')
+		if ((sim = ft_search(&s[i])))
 		{
-			if (s[i] != '.')
-				sim = s[i];
-			i++;
-		}
-		if (sim)
-		{
-			i -= 4;
 			while (s[i] && s[i] != '\n')
 			{
 				if (s[i] == sim || s[i + 5] == sim || s[i + 10] == sim)
@@ -157,15 +75,17 @@ char   *ft_add(char *s)
 			}
 			buf[j++] = '\n';
 			sim = 0;
+			i++;
 		}
-		i++;
+		else
+			i += 5;
 	}
 	return (buf);
 }
 
-tetrimino   *ft_newtet(char *s, int	x, int y, int	numb)
+tetrimino		*ft_newtet(char **s, int x, int y, int numb)
 {
-	tetrimino   *tmp;
+	tetrimino	*tmp;
 
 	tmp = (tetrimino*)ft_memalloc(sizeof(tetrimino));
 	tmp->numb = numb;
@@ -174,5 +94,4 @@ tetrimino   *ft_newtet(char *s, int	x, int y, int	numb)
 	tmp->content = s;
 	tmp->next = NULL;
 	return (tmp);
-
-}*/
+}
